34ee6c14fe7b86c81ab708c83084b82399ebf6d1
Aashvi Kothari <aashvikothari78@gmail.com>
2022-07-15 00:06:51 +0530

    Second Commit

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,7 +8,7 @@ export class NewsItem extends Component {
         let {title, description, imageUrl, newsUrl} = this.props;
         return (
             <div className='my-3'>
-                <div className="card" style={{width: "18rem"}}>
+                <div className="card">
                     <img src={!imageUrl?"https://images.moneycontrol.com/static-mcnews/2022/07/sensex_nifty1-1-770x433.jpg": imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
